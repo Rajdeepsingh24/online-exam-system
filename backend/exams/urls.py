d4c9b1f5266508d8ee257db7080e486ec1b6018e
Rajdeepsingh24 <141429275+Rajdeepsingh24@users.noreply.github.com>
2026-03-01 00:09:42 +0530

    .gitignore

--- backend/exams/urls.py
+++ backend/exams/urls.py
@@ -2,22 +2,49 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # Student
     path('exam/<int:exam_id>/', views.exam_detail, name='exam_detail'),
     path('exam/<int:exam_id>/result/', views.exam_result, name='exam_result'),
-    path('redirect/', views.role_redirect, name='role_redirect'),
     path('student-dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('student/exams/', views.available_exams, name='available_exams'),
+    path('student/results/', views.my_results, name='my_results'),
+
+    # Teacher
     path('teacher-dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
     path('teacher/exams/', views.teacher_exams, name='teacher_exams'),
     path('teacher/exams/create/', views.create_exam, name='create_exam'),
-    path('student/exams/', views.available_exams, name='available_exams'),
-    path('student/results/', views.my_results, name='my_results'),
-    path('start-exam/<int:exam_id>/', views.start_exam, name='start_exam'),
-    path('teacher/exams/<int:exam_id>/questions/', 
-     views.manage_questions, 
-     name='manage_questions'),
-     path(
-    'teacher/exams/<int:exam_id>/publish/',
-    views.publish_exam,
-    name='publish_exam'
+    path('teacher/exams/<int:exam_id>/questions/', views.manage_questions, name='manage_questions'),
+    path('teacher/exams/<int:exam_id>/publish/', views.publish_exam, name='publish_exam'),
+
+    # Common
+    path('redirect/', views.role_redirect, name='role_redirect'),
+    path(
+    'teacher/exam/<int:exam_id>/attempts/',
+    views.teacher_exam_attempts,
+    name='teacher_exam_attempts'
+),
+
+    path(
+    'teacher/attempt/<int:attempt_id>/detail/',
+    views.teacher_attempt_detail,
+    name='teacher_attempt_detail'
+),
+    path(
+    'teacher/exam/<int:exam_id>/edit/',
+    views.edit_exam,
+    name='edit_exam'
+),
+
+    path(
+    'teacher/exam/<int:exam_id>/delete/',
+    views.delete_exam,
+    name='delete_exam'
+),
+
+    path(
+    'teacher/exam/<int:exam_id>/duplicate/',
+    views.duplicate_exam,
+    name='duplicate_exam'
 ),
 ]
--- backend/exams/views.py
+++ backend/exams/views.py
@@ -1,15 +1,17 @@
+from urllib import request
+
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.http import HttpResponse
 from .models import Exam, Question, Attempt, StudentAnswer
 import json
-from datetime import datetime
+from datetime import timedelta
 from .models import Subject
-from django.db.models import Avg
-from django.db.models import Max
+from django.db.models import Avg, Count, Max
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
 
 
@@ -42,22 +44,71 @@ def student_dashboard(request):
         return redirect('login')
 
     exams = Exam.objects.filter(is_active=True)
-    attempts = Attempt.objects.filter(student=request.user, completed=True)
 
+    attempts = Attempt.objects.filter(
+        student=request.user,
+        completed=True
+    ).select_related('exam')
+
+    # Basic counts
     total_exams = exams.count()
     completed = attempts.count()
     pending = total_exams - completed
 
-    performance = 0
-    if total_exams > 0:
-        performance = int((completed / total_exams) * 100)
+    # ------------------------
+    # REAL PERFORMANCE LOGIC
+    # ------------------------
+    total_possible = 0
+    total_obtained = 0
 
+    for attempt in attempts:
+        total_possible += attempt.exam.total_marks
+        total_obtained += attempt.score
+
+    if total_possible > 0:
+        performance = int((total_obtained / total_possible) * 100)
+    else:
+        performance = 0
+
+    # ------------------------
+    # Recent attempts for chart
+    # ------------------------
     recent_attempts = attempts.order_by('-id')[:5]
 
-    # Chart Data
-    chart_labels = json.dumps([a.exam.title for a in recent_attempts][::-1])
-    chart_scores = json.dumps([a.score for a in recent_attempts][::-1])
+    chart_labels = json.dumps(
+        [a.exam.title for a in recent_attempts][::-1]
+    )
 
+    chart_scores = json.dumps(
+        [a.score for a in recent_attempts][::-1]
+    )
+
+    # ------------------------
+    # Leaderboard (latest exam)
+    # ------------------------
+    latest_exam = Exam.objects.order_by('-id').first()
+
+    leaderboard = []
+    user_rank = None
+
+    if latest_exam:
+        exam_attempts = Attempt.objects.filter(
+            exam=latest_exam,
+            completed=True
+        ).select_related('student').order_by('-score')
+
+        leaderboard = list(exam_attempts)
+
+        for index, attempt in enumerate(leaderboard):
+            if attempt.student == request.user:
+                user_rank = index + 1
+                break
+
+        leaderboard = leaderboard[:5]
+
+    # ------------------------
+    # Final Render
+    # ------------------------
     return render(request, 'dashboard/student_dashboard.html', {
         'exams': exams,
         'total_exams': total_exams,
@@ -67,36 +118,10 @@ def student_dashboard(request):
         'recent_attempts': recent_attempts,
         'chart_labels': chart_labels,
         'chart_scores': chart_scores,
-    })
-# Latest Exam
-    latest_exam = Exam.objects.order_by('-id').first()
-
-    leaderboard = []
-    user_rank = None
-
-    if latest_exam:
-        attempts = Attempt.objects.filter(
-            exam=latest_exam,
-            completed=True
-        ).select_related('student').order_by('-score')
-
-        leaderboard = list(attempts)
-
-        for index, attempt in enumerate(leaderboard):
-            if attempt.student == request.user:
-                user_rank = index + 1
-                break
-
-        leaderboard = leaderboard[:5]  # Top 5 only
-
-    context = {
-        # existing context...
         'leaderboard': leaderboard,
         'latest_exam': latest_exam,
         'user_rank': user_rank
-    }
-
-    return render(request, 'dashboard/student_dashboard.html', context)
+    })
 
 # ==========================================
 # TEACHER DASHBOARD
@@ -108,26 +133,73 @@ def teacher_dashboard(request):
         return HttpResponse("Access Denied")
 
     exams = Exam.objects.filter(created_by=request.user)
+
     exams_count = exams.count()
 
     questions_count = Question.objects.filter(
         exam__created_by=request.user
     ).count()
 
-    attempts = Attempt.objects.filter(
+    all_attempts = Attempt.objects.filter(
         exam__created_by=request.user,
         completed=True
-    ).order_by('-submitted_at')[:5]
+    )
 
-    attempts_count = attempts.count()
+    attempts_count = all_attempts.count()
+    avg_score = all_attempts.aggregate(avg=Avg('score'))['avg'] or 0
 
-    return render(request, 'dashboard/teacher_dashboard.html', {
+    # ======================
+    # Exam Analytics Table
+    # ======================
+    exam_data = []
+
+    for exam in exams:
+        total_questions = Question.objects.filter(exam=exam).count()
+
+        exam_attempts = Attempt.objects.filter(
+            exam=exam,
+            completed=True
+        )
+
+        total_attempts = exam_attempts.count()
+        exam_avg_score = exam_attempts.aggregate(avg=Avg('score'))['avg'] or 0
+        highest_score = exam_attempts.aggregate(max=Max('score'))['max'] or 0
+
+        exam_data.append({
+            'title': exam.title,
+            'questions': total_questions,
+            'attempts': total_attempts,
+            'avg_score': round(exam_avg_score, 2),
+            'highest_score': highest_score
+        })
+
+    # ======================
+    # Chart Data
+    # ======================
+    exam_titles = []
+    attempt_counts = []
+
+    for exam in exams:
+        exam_titles.append(exam.title)
+        count = Attempt.objects.filter(
+            exam=exam,
+            completed=True
+        ).count()
+        attempt_counts.append(count)
+
+    context = {
+        'exams': exams,
         'exams_count': exams_count,
         'questions_count': questions_count,
         'attempts_count': attempts_count,
-        'recent_attempts': attempts,
-    })
+        'avg_score': round(avg_score, 2),
+        'recent_attempts': all_attempts.order_by('-submitted_at')[:5],
+        'chart_labels': json.dumps(exam_titles),
+        'chart_data': json.dumps(attempt_counts),
+        'exam_data': exam_data
+    }
 
+    return render(request, 'dashboard/teacher_dashboard.html', context)
 
 # ==========================================
 # STUDENT EXAM VIEW
@@ -139,15 +211,18 @@ def exam_detail(request, exam_id):
         return HttpResponse("Access Denied")
 
     exam = get_object_or_404(Exam, id=exam_id)
-
     current_time = timezone.now()
 
-    # Time validation
+    # Exam window check
     if current_time < exam.start_time:
-        return HttpResponse("Exam has not started yet.")
+        return render(request, 'exams/exam_not_started.html', {
+    'exam': exam
+})
 
     if current_time > exam.end_time:
-        return HttpResponse("Exam time is over.")
+        return render(request, 'exams/exam_closed.html', {
+    'exam': exam
+})
 
     # Get or create attempt
     attempt, created = Attempt.objects.get_or_create(
@@ -155,14 +230,40 @@ def exam_detail(request, exam_id):
         exam=exam
     )
 
+    # If already completed
     if attempt.completed:
-        return render(request, 'exams/already_completed.html', {
-            'exam': exam
-        })
+        return redirect('exam_result', exam_id=exam.id)
+
+    # Calculate allowed end time based on duration
+    allowed_end_time = attempt.started_at + timedelta(
+        minutes=exam.duration_minutes
+    )
+
+    # If duration exceeded
+    if current_time > allowed_end_time:
+        attempt.completed = True
+        attempt.submitted_at = allowed_end_time
+        attempt.save()
+        return redirect('exam_result', exam_id=exam.id)
+
+    # Remaining time for frontend
+    remaining_seconds = int(
+        (allowed_end_time - current_time).total_seconds()
+    )
 
     questions = Question.objects.filter(exam=exam)
 
+    # ==========================
+    # POST (Submit Exam)
+    # ==========================
     if request.method == "POST":
+
+        # Backend time validation again (anti-hack)
+        if timezone.now() > allowed_end_time:
+            attempt.completed = True
+            attempt.submitted_at = allowed_end_time
+            attempt.save()
+            return redirect('exam_result', exam_id=exam.id)
 
         score = 0
 
@@ -170,8 +271,6 @@ def exam_detail(request, exam_id):
             selected_answer = request.POST.get(f"question_{question.id}")
 
             if selected_answer:
-
-                # Save or update answer safely
                 StudentAnswer.objects.update_or_create(
                     attempt=attempt,
                     question=question,
@@ -181,21 +280,21 @@ def exam_detail(request, exam_id):
                 if selected_answer == question.correct_answer:
                     score += question.marks
 
-        # Finalize attempt
         attempt.score = score
         attempt.completed = True
         attempt.submitted_at = timezone.now()
         attempt.save()
 
-        return render(request, 'exams/submission_success.html', {
-            'exam': exam
-        })
+        return redirect('exam_result', exam_id=exam.id)
 
+    # ==========================
+    # GET (Load Exam Page)
+    # ==========================
     return render(request, 'exams/exam_detail.html', {
         'exam': exam,
-        'questions': questions
+        'questions': questions,
+        'remaining_seconds': remaining_seconds
     })
-
 
 # ==========================================
 # RESULT VIEW
@@ -206,20 +305,35 @@ def exam_result(request, exam_id):
     if request.user.role != 'student':
         return HttpResponse("Access Denied")
 
-    try:
-        attempt = Attempt.objects.get(
-            student=request.user,
-            exam_id=exam_id
-        )
-    except Attempt.DoesNotExist:
-        return HttpResponse("Result not available.")
+    attempt = get_object_or_404(
+        Attempt,
+        student=request.user,
+        exam_id=exam_id,
+        completed=True
+    )
 
-    if not attempt.completed:
-        return HttpResponse("You have not completed this exam.")
+    total_marks = Question.objects.filter(
+        exam=attempt.exam
+    ).aggregate(total=Sum('marks'))['total'] or 0
 
-    return render(request, 'exams/result.html', {
-        'attempt': attempt
-    })
+    answers = StudentAnswer.objects.filter(
+        attempt=attempt
+    ).select_related('question')
+
+    correct_count = 0
+
+    for ans in answers:
+        if ans.selected_option == ans.question.correct_answer:
+            correct_count += 1
+
+    context = {
+        'attempt': attempt,
+        'total_marks': total_marks,
+        'answers': answers,
+        'correct_count': correct_count,
+    }
+
+    return render(request, 'exams/result.html', context)
 @login_required
 def teacher_exams(request):
 
@@ -228,8 +342,27 @@ def teacher_exams(request):
 
     exams = Exam.objects.filter(created_by=request.user)
 
-    return render(request, 'dashboard/teacher_exams.html', {
-        'exams': exams
+    exam_data = []
+
+    for exam in exams:
+        question_count = exam.question_set.count()
+
+        attempt_count = Attempt.objects.filter(
+            exam=exam,
+            completed=True
+        ).count()
+
+        exam_data.append({
+            "id": exam.id,
+            "title": exam.title,
+            "duration": exam.duration_minutes,
+            "is_active": exam.is_active,
+            "questions": question_count,
+            "attempts": attempt_count,
+        })
+
+    return render(request, "dashboard/teacher_exams.html", {
+        "exam_data": exam_data
     })
 @login_required
 def create_exam(request):
@@ -339,8 +472,116 @@ def my_results(request):
     return render(request, 'dashboard/my_results.html', {
         'attempts': attempts
     })
+@login_required
+def teacher_exam_attempts(request, exam_id):
+
+    if request.user.role != 'teacher':
+        return HttpResponse("Access Denied")
+
+    exam = get_object_or_404(Exam, id=exam_id, created_by=request.user)
+
+    attempts = Attempt.objects.filter(
+        exam=exam,
+        completed=True
+    ).select_related('student').order_by('-submitted_at')
+
+    return render(request, 'dashboard/teacher_exam_attempts.html', {
+        'exam': exam,
+        'attempts': attempts
+    })
+
 
 @login_required
-def start_exam(request, exam_id):
-    exam = get_object_or_404(Exam, id=exam_id)
-    return HttpResponse(f"You are starting exam: {exam.title}")
+def teacher_attempt_detail(request, attempt_id):
+
+    if request.user.role != 'teacher':
+        return HttpResponse("Access Denied")
+
+    attempt = get_object_or_404(
+        Attempt,
+        id=attempt_id,
+        exam__created_by=request.user
+    )
+
+    answers = StudentAnswer.objects.filter(
+        attempt=attempt
+    ).select_related('question')
+
+    return render(request, 'dashboard/teacher_attempt_detail.html', {
+        'attempt': attempt,
+        'answers': answers
+    })
+@login_required
+def edit_exam(request, exam_id):
+
+    if request.user.role != 'teacher':
+        return HttpResponse("Access Denied")
+
+    exam = get_object_or_404(
+        Exam,
+        id=exam_id,
+        created_by=request.user
+    )
+
+    subjects = Subject.objects.filter(created_by=request.user)
+
+    if request.method == "POST":
+        exam.title = request.POST.get('title')
+        exam.duration_minutes = request.POST.get('duration')
+        exam.total_marks = request.POST.get('total_marks')
+        exam.start_time = request.POST.get('start_time')
+        exam.end_time = request.POST.get('end_time')
+        exam.subject_id = request.POST.get('subject')
+        exam.save()
+
+        return redirect('teacher_exams')
+
+    return render(request, 'dashboard/edit_exam.html', {
+        'exam': exam,
+        'subjects': subjects
+    })
+@login_required
+def delete_exam(request, exam_id):
+
+    if request.user.role != 'teacher':
+        return HttpResponse("Access Denied")
+
+    exam = get_object_or_404(
+        Exam,
+        id=exam_id,
+        created_by=request.user
+    )
+
+    exam.delete()
+
+    return redirect('teacher_exams')
+@login_required
+def duplicate_exam(request, exam_id):
+
+    if request.user.role != 'teacher':
+        return HttpResponse("Access Denied")
+
+    exam = get_object_or_404(
+        Exam,
+        id=exam_id,
+        created_by=request.user
+    )
+
+    new_exam = Exam.objects.create(
+        title=exam.title + " (Copy)",
+        subject=exam.subject,
+        duration_minutes=exam.duration_minutes,
+        total_marks=exam.total_marks,
+        start_time=exam.start_time,
+        end_time=exam.end_time,
+        created_by=request.user,
+        is_active=False
+    )
+
+    # Copy questions
+    for question in exam.question_set.all():
+        question.pk = None
+        question.exam = new_exam
+        question.save()
+
+    return redirect('teacher_exams')
